473d93b60a45b13d86e459c978d7fed9ccef0253
hosseinGith <hosseinderees7@gmail.com>
2026-06-11 17:55:03 +0330

    no one can access to log with api

--- src/modules/auditLogsMedical/auditLogsMedical.controller.ts
+++ src/modules/auditLogsMedical/auditLogsMedical.controller.ts
@@ -1,36 +1,11 @@
-import { Body, Controller, Get, Param, Post, UseGuards } from '@nestjs/common';
-import { AuditLogsMedicalService } from './auditLogsMedical.service';
-import { ApiBearerAuth, ApiResponse } from '@nestjs/swagger';
+import { Controller, UseGuards } from '@nestjs/common';
+import { ApiBearerAuth } from '@nestjs/swagger';
 import { AuthGuard } from 'src/shared/guards/auth.guard';
 import { AccessType } from 'src/types';
-import AuditLogsMedicalDtoAdd from './dto/auditLogsMedical-add.dto';
-import { AuditLogsMedical } from 'src/modules/auditLogsMedical/entities/auditLogsMedical.entity';
 import { Access } from 'src/shared/decorators/access.decorator';
 import { AccessGuard } from 'src/shared/guards/access.guard';
 @Controller('/api/auditLogsMedical')
 @Access(AccessType.ADMIN)
 @UseGuards(AuthGuard, AccessGuard)
 @ApiBearerAuth()
-export class AuditLogsMedicalController {
- constructor(private readonly auditLogsMedical: AuditLogsMedicalService) {}
- @ApiResponse({
-  status: 200,
-  type: AuditLogsMedical,
- })
- @Get(':id')
- findOne(@Param('id') id: string) {
-  return this.auditLogsMedical.findOne(id);
- }
- @ApiResponse({
-  type: [AuditLogsMedical],
- })
- @Get()
- findAll() {
-  return this.auditLogsMedical.findAll();
- }
-
- @Post()
- create(@Body() body: AuditLogsMedicalDtoAdd) {
-  return this.auditLogsMedical.create(body);
- }
-}
+export class AuditLogsMedicalController {}
